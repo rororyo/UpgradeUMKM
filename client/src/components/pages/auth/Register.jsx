e8f325863f6cf0ac41e9ce44a44927127c7b48c8
Nathaniel Ryo <ryonathaniel05@gmail.com>
2024-03-03 00:03:04 +0700

    Finished Register page,WIP Login with google

--- client/src/components/pages/auth/Register.jsx
+++ client/src/components/pages/auth/Register.jsx
@@ -1,12 +1,16 @@
+//TODO: a litle resizing, bigger input text, show errors
 import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import axios from "axios";
 import Navbar from "../../partials/Navbar.jsx";
 
+//css imports
+import "/src/components/pages/auth/css/auth.css";
 const Register = () => {
   const [values, setValues] = useState({
     email: "",
     password: "",
+    repeatPassword:"",
   });
 
   const [notify, setNotify] = useState("");
@@ -26,61 +30,71 @@ const Register = () => {
     }
   }
   return (
-    <div style={{backgroundImage:'url("/assets/images/auth/authbg.png")',minHeight:'100vh'}}>
+    <div
+      style={{
+        backgroundImage: 'url("/assets/images/auth/authbg.png")',
+        minHeight: "100vh",
+      }}
+    >
       <Navbar />
-      <div className="container mt-5">
-        <h1>Register</h1>
-
-        <div className="row">
-          <div className="col-sm-8">
-            <div className="card">
-              <div className="card-body">
-                <form onSubmit={handleSubmit}>
-                  <div className="form-group">
-                    {notify && (
-                      <div className="alert alert-warning d-flex justify-content-between align-items-center">
-                        <div>{notify}</div>
-                        <button
-                          onClick={() => setNotify("")}
-                          className="btn-close"
-                        ></button>
-                      </div>
-                    )}
-                  </div>
-                  <div className="form-group">
-                    <label htmlFor="email">Email</label>
-                    <input
-                      type="email"
-                      className="form-control"
-                      name="email"
-                      onChange={(e) =>
-                        setValues({ ...values, email: e.target.value })
-                      }
-                    />
-                  </div>
-                  <div className="form-group">
-                    <label htmlFor="password">Password</label>
-                    <input
-                      type="password"
-                      className="form-control"
-                      name="password"
-                      onChange={(e) =>
-                        setValues({ ...values, password: e.target.value })
-                      }
-                    />
-                  </div>
-                  <div className="d-flex mt-4">
-                    <button type="submit" className="btn btn-dark">
-                      Register
+      <div
+        className="d-flex flex-column align-items-center justify-content-center"
+        style={{ minHeight: "85vh" }}
+      >
+        <span id="title-text">Siap upgrade usaha anda?</span>
+        <span id="subtitle-text">Buat akun dan rasakan manfaatnya!</span>
+        <div className="container mt-5" id="auth-container">
+<p className="text-center" id="auth-text">Register</p>
+          <div style={{ minHeight: "45vh" }} className="mt-5 d-flex flex-row">
+            <form className="ms-4 col-5 d-flex flex-column" onSubmit={handleSubmit}>
+              <label htmlFor="email" className="label-text">Email</label>
+              <input
+                type="email"
+                className="auth-input form-control"
+                name="email"
+                onChange={(e) =>
+                  setValues({ ...values, email: e.target.value })
+                }
+                placeholder="Enter your email" required
+              />
+              <label htmlFor="password" className="label-text mt-4">Password</label>
+              <input
+                type="password"
+                className="form-control auth-input"
+                name="password"
+                onChange={(e) =>
+                  setValues({ ...values, password: e.target.value })
+                }
+                placeholder="Enter your password" required
+              />
+              <label htmlFor="repeat-password" className="label-text mt-4">Repeat Password</label>
+              <input
+                type="password"
+                className="form-control auth-input"
+                name="repeat-password"
+                onChange={(e) =>
+                  setValues({ ...values, repeatPassword: e.target.value })
+                }
+                placeholder="Repeat your password" required
+              />
+               <button type="submit" className="btn mt-4"style={{minWidth:"95%"}} id="submit-button">
+                      <span id='submit-label'>Register</span>
                     </button>
-                    <Link to={"/login"} className="btn btn-dark ms-3">
-                      Login{" "}
-                    </Link>
-                  </div>
-                </form>
-              </div>
+            </form>
+
+            <div className="col-1 d-flex flex-column align-items-center ms-3">
+            <span id="or-text">OR</span>
+            <div class="vertical-line"></div>
+
+            </div>
+            <div className="col-5">
+<button className="auth-btn btn d-flex align-items-center">
+  <span id="third-party-label">Login with Google</span>
+  <img src="/assets/images/auth/googleLogo.jpeg" className="auth-logo ms-auto" />
+</button>
             </div>
           </div>
+
         </div>
       </div>
     </div>
